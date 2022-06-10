02a5dd85cffcaeb6c105e83bd18d98aa9b72c6fd
Imtiaz Modder <67188507+Imtiaz-Modder@users.noreply.github.com>
2022-06-10 14:09:20 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/corvus_merlinx.mk
+   $(LOCAL_DIR)/xdroid_merlinx.mk
 
 COMMON_LUNCH_CHOICES := \
-    corvus_merlinx-user \
-    corvus_merlinx-userdebug \
-    corvus_merlinx-eng
+    xdroid_merlinx-user \
+    xdroid_merlinx-userdebug \
+    xdroid_merlinx-eng
